4ba2e76ada32134d841b43421bf51eb03fe38d7c
Sylvester Roos <s.roos@nerds.company>
2026-07-15 13:53:38 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Petri.MixProject do
   def project do
     [
       app: :petri,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
